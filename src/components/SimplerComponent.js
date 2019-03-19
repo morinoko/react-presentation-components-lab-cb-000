cb0cd8f3c9a186db4633cdb56bfbb86fad0286bc
Felice Forby <feliceforby@gmail.com>
2019-03-19 01:02:59 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,6 +1,7 @@
 // Code SimplerComponent Here
 import React from 'react';
 
+// A stateless functional component. Only changes via props from parent.
 const SimplerComponent = (props) => 
   <div onClick={props.handleClick}> I am just happy.</div>;
 
